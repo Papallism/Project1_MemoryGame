065913e0f52b18c5c63ee9a0ec094b9e47d881ad
Michalis Papallis <mihalispapallis@gmail.com>
2019-03-17 16:08:26 +0200

    Add files via upload

--- Project1.cpp
+++ Project1.cpp
@@ -4,27 +4,36 @@
 #include <Windows.h>
 using namespace std;
 
-void displayBoard();
+const int BOARDSIZE = 3;
+
+void displayBoard(char [][BOARDSIZE]);
 void displayPositions();
 
 int main()
 {
-    const int SIZE = 3;
-    char board[SIZE][SIZE] = {'a', 'd', 'b', 'a', 'c', ' ', 'd', 'b', 'c'};
+    int moves = 0;
+    char stars[BOARDSIZE][BOARDSIZE] = {'*', '*', '*', '*', '*', '*', '*', '*', '*'};
+    char board[BOARDSIZE][BOARDSIZE] = {'a', 'd', 'b', 'a', 'c', ' ', 'd', 'b', 'c'};
 
     cout << "Welcome to the Matching Game. At each move, choose two positions that you think match.\n";
-    displayBoard();
+    displayBoard(stars);
     displayPositions();
 
     return 0;
 }
 
-void displayBoard()
+void displayBoard(char stars[][BOARDSIZE])
 {
-    cout << "\nBoard is:\n";
+    cout << "\nBoard is:\n\n";
+    for(int i = 0; i < BOARDSIZE; i++)
+    {
+        for(int j = 0; j < BOARDSIZE; j++)
+            cout << stars[i][j] << "  ";
+        cout << endl;
+    }
 }
 
 void displayPositions()
 {
-    cout << "\nBoard positions are:\n\n1 2 3 \n4 5 6\n7 8 9\n";
+    cout << "\nBoard positions are:\n\n1  2  3 \n4  5  6\n7  8  9\n";
 }
