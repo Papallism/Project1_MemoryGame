994eb808b75552b143b017e599174e5c216023da
Michalis Papallis <mihalispapallis@gmail.com>
2019-03-19 14:00:37 +0200

    Add files via upload

--- Project1.cpp
+++ Project1.cpp
@@ -1,39 +1,91 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <ctime>
 #include <Windows.h>
 using namespace std;
 
-const int BOARDSIZE = 3;
+const int BOARDSIZE = 3; //constant for board dimensions
 
-void displayBoard(char [][BOARDSIZE]);
+void displayStars(char [][BOARDSIZE]);
 void displayPositions();
+void displayChoice(const char [][BOARDSIZE], const int , const int);
 
 int main()
 {
-    int moves = 0;
+    bool win = false;
+    int pos1, pos2, moves = 0;
     char stars[BOARDSIZE][BOARDSIZE] = {'*', '*', '*', '*', '*', '*', '*', '*', '*'};
     char board[BOARDSIZE][BOARDSIZE] = {'a', 'd', 'b', 'a', 'c', ' ', 'd', 'b', 'c'};
 
     cout << "Welcome to the Matching Game. At each move, choose two positions that you think match.\n";
-    displayBoard(stars);
     displayPositions();
+    while(!win)
+    {
+        moves++;
+        do
+        {
+            cout << "\nMove #" << moves << " : Enter two positions to match: ";
+            cin >> pos1;
+            cin >> pos2;
+            if((pos1 < 1 || pos1 > 9) || (pos2 < 1 || pos2 > 9) || pos1 == pos2)
+                cout << "Invalid input! Please enter valid positions.\n";
+        }while((pos1 < 1 || pos1 > 9) || (pos2 < 1 || pos2 > 9) || pos1 == pos2);
+        displayChoice(board, pos1, pos2);
+        displayStars(stars);
+    }
 
     return 0;
 }
 
-void displayBoard(char stars[][BOARDSIZE])
+void displayPositions()
+{
+    cout << "\nBoard is:\n\n*  *  *\n*  *  *\n*  *  *\n";
+    cout << "\nBoard positions are:\n\n1  2  3 \n4  5  6\n7  8  9\n";
+}
+
+void displayChoice(const char board[][BOARDSIZE], const int choice1, const int choice2)
+{//displays board after selection
+    cout << "\nBoard is:\n\n";
+    for(int i = 0; i < BOARDSIZE; i++)
+    {
+        for(int j = 0; j < BOARDSIZE; j++)
+        {
+            if((i == (choice1 - 1) / BOARDSIZE) && (j == (choice1 - 1) % BOARDSIZE))
+                cout << setw(3) << left << board[i][j];
+            else if((i == (choice2 - 1) / BOARDSIZE) && (j == (choice2 - 1) % BOARDSIZE))
+                cout << setw(3) << left << board[i][j];
+            else
+                cout << setw(3) << left << "*";
+        }
+        cout << endl;
+    }
+}
+
+void displayStars(char starsBoard [][BOARDSIZE])
 {
     cout << "\nBoard is:\n\n";
     for(int i = 0; i < BOARDSIZE; i++)
     {
         for(int j = 0; j < BOARDSIZE; j++)
-            cout << stars[i][j] << "  ";
+            cout << setw(3) << left << starsBoard[i][j];
         cout << endl;
     }
 }
 
-void displayPositions()
-{
-    cout << "\nBoard positions are:\n\n1  2  3 \n4  5  6\n7  8  9\n";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
